d7d2c5b47ec64b810d0ddd27f6cc200b223287a9
Abhijith S Parackal <abhijith.s.parackal@gmail.com>
2019-11-26 22:32:08 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -7,12 +7,12 @@ notify=Notify()
 
 url = "http://www.dota2.com/news/updates/"   #Your URL here
 
-sleeptime = 300
+sleeptime = 300    #sleeptime
 def getHash():
     r = urllib.request.urlopen(url)
     the_page = r.read()
     return hashlib.sha224(the_page).hexdigest()
-current_hash = getHash()
+current_hash = getHash()      # much update needed here
 
 while 1:
     if getHash() == current_hash:

2ba4ae88e1c41ec323face67cdbb938e9c1b2ade
Abhijith S Parackal <abhijith.s.parackal@gmail.com>
2019-11-26 22:23:42 +0530

    Create Run.py
    First, commit

---
+++ Run.py
@@ -0,0 +1,23 @@
+import hashlib
+from notify_run import Notify
+import time
+import datetime
+import urllib
+notify=Notify()
+
+url = "http://www.dota2.com/news/updates/"   #Your URL here
+
+sleeptime = 300
+def getHash():
+    r = urllib.request.urlopen(url)
+    the_page = r.read()
+    return hashlib.sha224(the_page).hexdigest()
+current_hash = getHash()
+
+while 1:
+    if getHash() == current_hash:
+        print('Nothing')
+    else:
+        notify.send(f"New Patch Out at {datetime.datetime.now()}")
+        break
+    time.sleep(sleeptime)
